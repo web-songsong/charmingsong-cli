25895c6aed956b1d900acd2348b21c8c82ba4e31
charmingsong <web_songsong@163.com>
2019-01-08 19:12:16 +0800

    Update init.js

--- info-data/init.js
+++ info-data/init.js
@@ -4,14 +4,14 @@ module.exports = [
     message: '选择模板',
     choices: ['xs-templates', 'manage-system', 'blogProject'],
     name: 'template'
-  },
-  {
-    type: 'input',
-    message: '项目名称(英文)',
-    name: 'name',
-    validate(val) {
-      if (/^\w+$/.test(val)) return true
-      return '英文!!!'
-    }
   }
+  // {
+  //   type: 'input',
+  //   message: '项目名称(英文)',
+  //   name: 'name',
+  //   validate(val) {
+  //     if (/^\w+$/.test(val)) return true
+  //     return '英文!!!'
+  //   }
+  // }
 ]
