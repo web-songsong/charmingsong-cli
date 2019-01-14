6cbfa08cc5f68041182be65047e1971ba47a7e26
charmingsong <web_songsong@163.com>
2019-01-14 20:37:32 +0800

    cli 输出文件

---
+++ meta/init.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    type: 'list',
+    message: '选择模板',
+    choices: ['xs-templates', 'manage-system', 'blogProject'],
+    name: 'template'
+  },
+  {
+    type: 'input',
+    message: '项目名称(英文)',
+    name: 'name',
+    validate(val) {
+      if (/^\w+$/.test(val)) return true
+      return '英文!!!'
+    }
+  }
+]
