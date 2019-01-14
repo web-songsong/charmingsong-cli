76bff79cca3580a9494636921d166780e557f91a
charmingsong <web_songsong@163.com>
2019-01-15 00:05:15 +0800

    v1..1.0

--- meta/init.js
+++ meta/init.js
@@ -1,17 +1,19 @@
-module.exports = [
-  {
-    type: 'list',
-    message: '选择模板',
-    choices: ['xs-templates', 'manage-system', 'blogProject'],
-    name: 'template'
-  },
-  {
-    type: 'input',
-    message: '项目名称(英文)',
-    name: 'name',
-    validate(val) {
-      if (/^\w+$/.test(val)) return true
-      return '英文!!!'
+module.exports = {
+  prompts: [
+    {
+      type: 'list',
+      message: '选择模板',
+      choices: ['xs-templates', 'manage-system', 'blogProject'],
+      name: 'template'
     }
-  }
-]
+    // {
+    //   type: 'input',
+    //   message: '项目名称(英文)',
+    //   name: 'name',
+    //   validate(val) {
+    //     if (/^\w+$/.test(val)) return true
+    //     return '英文!!!'
+    //   }
+    // }
+  ]
+}
