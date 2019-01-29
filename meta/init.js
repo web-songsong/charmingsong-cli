8f31acc0098d67c2297f988794572b61f0cbadf0
charmingsong <web_songsong@163.com>
2019-01-29 14:34:49 +0800

    template name update

--- meta/init.js
+++ meta/init.js
@@ -2,24 +2,24 @@ module.exports = {
   prompts: [
     {
       type: 'list',
-      message: '选择模板',
-      choices: ['xs-templates', 'manage-system', 'blogProject'],
+      message: '选择模板(select template)',
+      choices: ['templates-ui'],
       name: 'template'
     },
     {
       type: 'input',
-      message: '项目名称',
+      message: '项目名称(project name)',
       name: 'projectName',
       default: 'myProject'
     },
     {
       type: 'input',
-      message: '简介',
+      message: '简介(description)',
       name: 'description'
     },
     {
       type: 'input',
-      message: '版本号',
+      message: '版本号(version)',
       default: '0.0.1',
       name: 'version'
     }
