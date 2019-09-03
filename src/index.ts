0866f9f7d878b32e6bb44d9d8565b55c9cc78acd
charmingsong <web_songsong@163.com>
2019-09-03 23:42:52 +0800

    program  开发

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,12 @@ import * as package_info from '../package.json'
 import * as prompt from './libs/inquirer'
 
 import Logger = require('./libs/logger')
-Logger.fatal()
 
-// console.log(log)
-// // program.version(package_info.version, '-v, --version')
-// // program
-// //   .command('init <template> [otherDirs...]')
-// //   .action(function(d: string, otherD: any) {
-// //     if (otherD.length) return log.fatal('参数错误')
-// //   })
+program.version(package_info.version, '-v, --version')
+program
+  .command('init <template> [otherDirs...]')
+  .action(function(template: string, otherD: any) {
+    if (otherD.length) return Logger.fatal('参数错误')
+  })
 
-// // program.parse(process.argv)
-
-console.log('=======')
+program.parse(process.argv)
